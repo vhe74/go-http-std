5a922dddc80bd524de593543c4fc18aa56e5cdfa
Vincent Heuschling <vhe@affini-tech.com>
2024-03-29 20:28:22 +0100

    Separates Handlers

--- main.go
+++ main.go
@@ -10,23 +10,28 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/task/{id}/", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		log.Printf("200 GET /task/%s", id)
-		fmt.Fprintf(w, "handling task with id=%v\n", id)
-	})
-
-	mux.HandleFunc("/task/{id}/status", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		log.Printf("200 GET /task/%s/status", id)
-		fmt.Fprintf(w, "handling task status with id=%v\n", id)
-	})
-
-	mux.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("200 GET /home")
-		fmt.Fprint(w, "home\n")
-	})
+	mux.HandleFunc("/task/{id}/", handleTaskByID)
+	mux.HandleFunc("/task/{id}/status", handleTaskStatusByID)
+	mux.HandleFunc("GET /home", handleHome)
 
 	log.Default().Println("Booting server")
 	http.ListenAndServe("localhost:8090", mux)
+}
+
+// Handlers
+func handleTaskByID(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	log.Printf("200 GET /task/%s", id)
+	fmt.Fprintf(w, "handling task with id=%v\n", id)
+}
+
+func handleTaskStatusByID(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	log.Printf("200 GET /task/%s/status", id)
+	fmt.Fprintf(w, "handling task status with id=%v\n", id)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	log.Println("200 GET /home")
+	fmt.Fprint(w, "home\n")
 }
